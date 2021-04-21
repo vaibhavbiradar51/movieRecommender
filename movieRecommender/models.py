d4e032ee7a2f84bf4d0148df743f3012ca62a02b
Pulkit-Marlin <pulkitagrawal2000@gmail.com>
2021-04-21 23:51:28 +0530

    "working"

--- movieRecommender/models.py
+++ movieRecommender/models.py
@@ -17,11 +17,11 @@ class User:
 
     def find(self):
         matcher = NodeMatcher(graph)
-        return len(matcher.match("User", email=self.email))
+        return (matcher.match("User", email=self.email)).first()
 
-    def signup(self, password):
+    def signup(self, name, password):
         if not self.find():
-            user = Node('User', email=self.email, password=bcrypt.encrypt(password))
+            user = Node('User', email=self.email, name=name, password=bcrypt.encrypt(password))
             graph.create(user)
             return True
         else:
@@ -30,8 +30,97 @@ class User:
     def verify_password(self, password):
         user = self.find()
         if user:
-            print("here")
-            # return bcrypt.verify(password, user['password'])
+            return bcrypt.verify(password, user['password'])
             return True
         else:
             return False
+
+class Movie:
+    def __init__(self, title, year, criticsRating):
+        self.title = title
+        self.year = year
+        self.criticsRating = criticsRating
+
+class Genre:
+    def __init__(self, genre):
+        self.genre = genre
+
+    def find(self):
+        matcher = NodeMatcher(graph)
+        return (matcher.match("Genre", genre=self.genre)).first()
+
+    def add(self):
+        if not self.find():
+            genreNode = Node('Genre', genre=self.genre)
+            graph.create(genreNode)
+            return True
+        else:
+            return False
+
+def getAllGenreSerialized():
+    query = '''
+    MATCH (g:Genre)
+    RETURN g
+    '''
+
+    allGenres = graph.run(query)
+    serializedAllGenres = []
+    for record in allGenres:
+        g = record['g']
+        serializedAllGenres.append({
+            'id': g.identity,
+            'genre': g['genre'],
+        })
+
+    return serializedAllGenres
+
+class Country:
+    def __init__(self, country):
+        self.country = country
+
+    def find(self):
+        matcher = NodeMatcher(graph)
+        return (matcher.match("Country", country=self.country)).first()
+
+    def add(self):
+        if not self.find():
+            countryNode = Node('Country', country=self.country)
+            graph.create(countryNode)
+            return True
+        else:
+            return False
+
+def getAllCountrySerialized():
+    query = '''
+    MATCH (c:Country)
+    RETURN c
+    '''
+
+    allCountrys = graph.run(query)
+    serializedAllCountrys = []
+    for record in allCountrys:
+        c = record['c']
+        serializedAllCountrys.append({
+            'id': c.identity,
+            'country': c['country'],
+        })
+
+    return serializedAllCountrys
+
+class Actor:
+    def __init__(self, name):
+        self.name = name
+
+    def add(self):
+        actorNode = Node('Actor', name=self.name)
+        graph.create(actorNode)
+        return
+
+class Director:
+    def __init__(self, name):
+        self.name = name
+
+    def add(self):
+        directorNode = Node('Director', name=self.name)
+        graph.create(directorNode)
+        return
--- movieRecommender/views.py
+++ movieRecommender/views.py
@@ -1,4 +1,4 @@
-from .models import User
+from .models import *
 from flask import Flask, request, session, redirect, url_for, render_template, flash
 
 app = Flask(__name__)
@@ -7,17 +7,21 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+# (1) Signup
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     if request.method == 'POST':
+        name = request.form['name']
         email = request.form['email']
         password = request.form['password']
 
         if len(email) < 1:
             flash('Your email must be at least one character.')
+        elif len(name) < 1:
+            flash('Your name must be at least one character.')
         elif len(password) < 5:
             flash('Your password must be at least 5 characters.')
-        elif not User(email).signup(password):
+        elif not User(email).signup(name, password):
             flash('A user with that email already exists.')
         else:
             session['email'] = email
@@ -26,13 +30,12 @@ def signup():
 
     return render_template('signup.html')
 
+# (2) Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
-        print(email, password)
 
         if not User(email).verify_password(password):
             flash('Invalid login.')
@@ -42,3 +45,54 @@ def login():
             return redirect(url_for('hello'))
 
     return render_template('login.html')
+
+
+# (16) Staff create new preference
+@app.route('/createPreference', methods=['GET', 'POST'])
+def createPreference():
+    if request.method == 'POST':
+        if 'genre' in request.form:
+            genre = request.form['genre']
+            if len(genre) < 1:
+                flash('Genre must be atleast 1 character')
+            elif not Genre(genre).add():
+                flash('Genre already exists')
+            else:
+                return redirect(url_for('createPreference'))
+        
+        elif 'country' in request.form:
+            country = request.form['country']
+            if len(country) < 1:
+                flash('Country of Origin must be atleast 1 character')
+            elif not Country(country).add():
+                flash('Country already exists')
+            else:
+                return redirect(url_for('createPreference'))
+        
+        elif 'actor' in request.form:
+            actor = request.form['actor']
+            if len(actor) < 1:
+                flash('Actor Name must be atleast 1 character')
+            else:
+                Actor(actor).add()
+                return redirect(url_for('createPreference'))
+        
+        elif 'director' in request.form:
+            director = request.form['director']
+            if len(director) < 1:
+                flash('Director Name must be atleast 1 character')
+            else:
+                Director(director).add()
+                return redirect(url_for('createPreference'))
+
+    return render_template('createPreference.html')
+
+
+# (17) Creating New Movie
+@app.route('/createMovie', methods=['GET', 'POST'])
+def createMovie():
+    if request.method == 'POST':
+        print(request.form)
+        print(request.form.getlist('genre'))
+    
+    return render_template('createMovie.html', genres=getAllGenreSerialized(), countries=getAllCountrySerialized()) 
