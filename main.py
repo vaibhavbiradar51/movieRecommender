687e7408f3851fbdd8ebc4e83d9638e1ac7c27d2
vaibhavbiradar51 <48184786+vaibhavbiradar51@users.noreply.github.com>
2021-05-14 10:04:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from movieRecommender import app
 import os
 
+
 app.secret_key = os.urandom(24)
-app.run(host="boomsi.azurewebsites.net",debug=True)           
+app.run(host='0.0.0.0',port=8000, debug=True)           
